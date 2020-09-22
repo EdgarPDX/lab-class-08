3c916978a9843a6640685a58bd08643bd58d3ee6
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-09-21 17:44:02 -0700

    finished / endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('should respond with "hi"', async() => {
+    const res = await request(app)
+      .get('/');
 
+    
+    expect(res.text).toEqual('Hi');
+  });
 });
